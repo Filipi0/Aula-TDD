9ec8375bf3ca27c839bc7669ab262e5b32a4cf3d
Filipi Dantas <127785380+Filipi0@users.noreply.github.com>
2025-03-22 10:19:27 -0300

    Refatora a classe Money para incluir métodos estáticos de criação para Dollar e Franc. Atualiza as classes Dollar e Franc para estender Money e implementa o método times. Modifica os testes para usar a nova estrutura.

--- src/main/java/com/money/Dollar.java
+++ src/main/java/com/money/Dollar.java
@@ -1,20 +1,11 @@
 package com.money;
 
-
-public class Dollar {
-    private int amount;
-
+public class Dollar extends Money {
     Dollar(int amount) {
         this.amount = amount;
     }
 
-    Dollar times(int multiplier) {
+    public Money times(int multiplier) {
         return new Dollar(amount * multiplier);
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        Dollar dollar = (Dollar) object;
-        return amount == dollar.amount;
     }
 }
---
+++ src/main/java/com/money/Money.java
@@ -0,0 +1,21 @@
+package com.money;
+
+public abstract class Money {
+    protected int amount;
+
+    static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    static Money franc(int amount) {
+        return new Franc(amount);
+    }
+
+    public abstract Money times(int multiplier);
+
+    @Override
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount && getClass().equals(money.getClass());
+    }
+}
