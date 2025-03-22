4020a4bac83e9059775ecf14e5656eeef2113846
Filipi Dantas <127785380+Filipi0@users.noreply.github.com>
2025-03-22 09:59:11 -0300

    Atualiza a classe Dollar para inicializar o valor da quantia no construtor e modifica o método times para atualizar a quantia corretamente.

--- src/main/java/com/money/Dollar.java
+++ src/main/java/com/money/Dollar.java
@@ -1,11 +1,11 @@
-package com.money;
-
 public class Dollar {
-    int amount = 10;
+    int amount;
 
     Dollar(int amount) {
+        this.amount = amount;
     }
 
     void times(int multiplier) {
+        amount = amount * multiplier;
     }
 }
